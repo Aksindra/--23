024f5c909c6015f7e4c19354427f7d3777af4d1f
Aksindra <sashaatt234@gmail.com>
2024-12-19 09:04:17 +0200

    new 4

---
+++ script.js
@@ -0,0 +1,18 @@
+
+console.log("Привіт, це повідомлення з JavaScript!");
+
+
+alert("Ласкаво просимо на сторінку з JavaScript!");
+
+const num1 = 8;
+const num2 = 5;
+
+
+const sum = num1 + num2;
+
+
+const difference = num1 - num2;
+
+
+console.log(`Сума: ${sum}`);
+console.log(`Різниця: ${difference}`);
